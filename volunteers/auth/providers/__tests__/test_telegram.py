9d9e657fb76e8769d0837bf42a2ab2965c07efb4
Kolesov Matvey <kolesov645@gmail.com>
2025-05-02 12:51:34 +0300

    Unit tests now use pytest fixtures for dependency injection

--- volunteers/auth/providers/__tests__/test_telegram.py
+++ volunteers/auth/providers/__tests__/test_telegram.py
@@ -1,11 +1,31 @@
 import copy
-import os
+from collections.abc import Generator
+
+import dependency_injector.containers as containers
+import dependency_injector.providers as providers
+import pytest
 
 from volunteers.auth.providers.telegram import TelegramLoginData, verify_telegram_login_hash
+from volunteers.core.config import Config
+
+
+class Container(containers.DeclarativeContainer):
+    config = providers.Singleton(Config)
+
+
+@pytest.fixture(scope="session")
+def container() -> Generator[Container]:
+    container = Container()
+    yield container
+    container.unwire()
+
+
+@pytest.fixture(scope="session")
+def token(container: Container) -> str:
+    return container.config().telegram.token
 
 
 class TestTelegram:
-    token: str = os.environ.get("TG_TOKEN", "")
     test_data = TelegramLoginData(
         auth_date=1746113463,
         first_name="Матвей",
@@ -16,11 +36,11 @@ class TestTelegram:
         hash="494e35602ffba396978394e8d1f58bc00d098070366d3300acacdfadee75f26e",
     )
 
-    def test_valid_login(self) -> None:
+    def test_valid_login(self, token: str) -> None:
         data = self.test_data
-        assert verify_telegram_login_hash(data, self.token)
+        assert verify_telegram_login_hash(data, token)
 
-    def test_invalid_login(self) -> None:
+    def test_invalid_login(self, token: str) -> None:
         data = copy.deepcopy(self.test_data)
         data.first_name += "!"
-        assert not verify_telegram_login_hash(data, self.token)
+        assert not verify_telegram_login_hash(data, token)
